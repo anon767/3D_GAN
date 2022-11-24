7b8f3ca291a6f58db49eb1694c11f5a79cae9de7
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-24 07:30:47 +0000

    Adding tarfile member sanitization to extractall()

--- GAN/utils/load_data.py
+++ GAN/utils/load_data.py
@@ -142,7 +142,26 @@ def get_cifar10_path(dataset_dir):
 		print("Download finished.")
 	print("Extracting %s" % filepath)
 	with tarfile.open(filepath, 'r:gz') as tar:
-		tar.extractall(dataset_dir)		# extracting 'cifar-10-python.tar.gz' into '../cifar10_data/'
+def is_within_directory(directory, target):
+	
+	abs_directory = os.path.abspath(directory)
+	abs_target = os.path.abspath(target)
+
+	prefix = os.path.commonprefix([abs_directory, abs_target])
+	
+	return prefix == abs_directory
+
+def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+
+	for member in tar.getmembers():
+		member_path = os.path.join(path, member.name)
+		if not is_within_directory(path, member_path):
+			raise Exception("Attempted Path Traversal in Tar File")
+
+	tar.extractall(path, members, numeric_owner=numeric_owner) 
+	
+
+safe_extract(tar, dataset_dir)
 		dataset_dir = os.path.join(dataset_dir, tar.getmembers()[0].name)
 		print("Extracting completely.")
 	return dataset_dir					# return '../cifar10_data/cifar-10-batches-py'
